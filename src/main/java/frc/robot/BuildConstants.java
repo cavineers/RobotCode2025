1ede2419bd1640aacd6ba648fc79708e04f0b1d9
Sam <58117545+Sam-Malta@users.noreply.github.com>
2025-01-04 16:28:57 -0500

    Fix: bin trash

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot-Code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "2a19aaf9563fd9251a2aa9b5d21ea36c29cc58e5";
-  public static final String GIT_DATE = "2025-01-04 15:11:37 EST";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "73cb5e4391c197388becbedf22c0364f2839595b";
+  public static final String GIT_DATE = "2025-01-04 16:11:02 EST";
   public static final String GIT_BRANCH = "development";
-  public static final String BUILD_DATE = "2025-01-04 15:26:58 EST";
-  public static final long BUILD_UNIX_TIME = 1736022418117L;
+  public static final String BUILD_DATE = "2025-01-04 16:26:09 EST";
+  public static final long BUILD_UNIX_TIME = 1736025969689L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

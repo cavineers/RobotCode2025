2a19aaf9563fd9251a2aa9b5d21ea36c29cc58e5
Sam <58117545+Sam-Malta@users.noreply.github.com>
2025-01-04 15:11:37 -0500

    Feat: current commit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Robot-Code-Framework-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "f392744ebd0f8c7c506d64edbc6cf0f7c1e72c6d";
+  public static final String GIT_DATE = "2024-12-27 21:23:15 EST";
+  public static final String GIT_BRANCH = "development";
+  public static final String BUILD_DATE = "2025-01-03 15:57:42 EST";
+  public static final long BUILD_UNIX_TIME = 1735937862070L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}

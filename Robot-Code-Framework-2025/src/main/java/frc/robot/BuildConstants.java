39007cabf0a05c91e025ffd8bb708babf990348e
Sam <58117545+Sam-Malta@users.noreply.github.com>
2025-01-04 17:59:52 -0500

    Reset some files and vendorops

---
+++ Robot-Code-Framework-2025/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Robot-Code-Framework-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "36f70c804375b383a5387364ad85b3eb1cb47dc3";
+  public static final String GIT_DATE = "2025-01-04 16:43:28 EST";
+  public static final String GIT_BRANCH = "development";
+  public static final String BUILD_DATE = "2025-01-04 17:45:58 EST";
+  public static final long BUILD_UNIX_TIME = 1736030758799L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}

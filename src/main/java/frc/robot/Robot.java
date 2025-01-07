702f6ed980bf948b2f82e0f6e3b843535b7bdabf
Sam <58117545+Sam-Malta@users.noreply.github.com>
2025-01-06 23:03:38 -0500

    FixL build constant import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.constants.BuildConstants;
 import frc.robot.constants.SimConstants;
 
 /**

7aaa8f14b5ff8ab38e6a00aff3a235029943a1b8
mattheW0324 <lindquistma27@spaldinghs.org>
2025-01-06 22:09:43 -0500

    Feat: Actually divided it

---
+++ src/main/java/frc/robot/constants/RemoteConstants.java
@@ -0,0 +1,14 @@
+package frc.robot.constants;
+
+public class RemoteConstants {
+    public static final class OIConstants {
+        public static final int kDriverJoystickPort = 0;
+
+        public static final int kDriverYAxis = 1;
+        public static final int kDriverXAxis = 0;
+        public static final int kDriverRotAxis = 4;
+        public static final int kDriverFieldOrientedButtonIdx = 1;
+
+        public static final double kDeadband = 0.1;
+    }
+}
---
+++ src/main/java/frc/robot/constants/SensorConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.constants;
+
+public class SensorConstants {
+    public static final class DIO {
+    }
+    // Turn encoder configuration
+    public static final boolean turnEncoderInverted = true;
+    // Check the conversion factors
+}

538678ff4b0bc35694e0aacd6c83ee221bd2099a
mattheW0324 <lindquistma27@spaldinghs.org>
2025-01-16 18:35:03 -0500

    Fix: Gradle weird

--- src/main/java/frc/robot/subsystems/Drivetrain/ModuleIOSpark.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/ModuleIOSpark.java
@@ -16,7 +16,6 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.Debouncer;

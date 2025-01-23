7488b2c5a2bb3910810d87578b8438ce1911a80d
Sam <58117545+Sam-Malta@users.noreply.github.com>
2025-01-23 16:57:22 -0500

    Fix: real adapter

--- src/main/java/frc/robot/subsystems/Algaebar/AlgaeBarConstants.java
+++ src/main/java/frc/robot/subsystems/Algaebar/AlgaeBarConstants.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems.Algaebar;
 
 public class AlgaeBarConstants {
-    public static final int kAlgaeBarCoralCanID = 1;
-    public static final int kAlgaeBarRotateCanID = 2;
+    public static final int kAlgaeBarCoralCanID = 50;
+    public static final int kAlgaeBarRotateCanID = 51;
 
     public static final double minRotation = 0; //Pretty much all of these are TBD since I still have no clue what I'm doing
     public static final double maxRotation = 0; 

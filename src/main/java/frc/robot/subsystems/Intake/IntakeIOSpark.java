8515285a74d99122fc8523bd5bb512dc691baf46
Carmen <149629166+carmen-contreras@users.noreply.github.com>
2025-01-25 12:09:19 -0500

    sensor addition + config
    added sensors + configs

--- src/main/java/frc/robot/subsystems/Intake/IntakeIOSpark.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIOSpark.java
@@ -1,12 +1,14 @@
 package frc.robot.subsystems.Intake;
 
 
-    import java.util.function.DoubleSupplier;
     import static frc.lib.SparkUtil.*;
     
     import com.revrobotics.RelativeEncoder;
     import com.revrobotics.spark.SparkLowLevel.MotorType;
-    import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import com.revrobotics.spark.SparkMax;
     import static frc.robot.subsystems.Intake.IntakeConstants.kLeftIntakeCanID;
     import static frc.robot.subsystems.Intake.IntakeConstants.kRightIntakeCanID;
     
@@ -17,25 +19,15 @@ package frc.robot.subsystems.Intake;
         private final SparkMax rightMotor = new SparkMax(kRightIntakeCanID, MotorType.kBrushless);
         private final RelativeEncoder leftEncoder = leftMotor.getEncoder();
         private final RelativeEncoder rightEncoder = rightMotor.getEncoder();
+        public DigitalInput leftSensor = new DigitalInput(IntakeConstants.CoralSensorLeft);
+        public DigitalInput rightSensor = new DigitalInput(IntakeConstants.CoralSensorRight);
+
     
         public IntakeIOSpark(){
             //motor config?
         }
     
-        public void updateInputs(IntakeIOInputs inputs){
-            ifOk(leftMotor, leftEncoder::getPosition, value -> inputs.positionRad = value);
-            ifOk(rightMotor, rightEncoder::getVelocity, value -> inputs.velocityRadPerSec = value);
-            ifOk(
-            leftMotor,
-                new DoubleSupplier[] {leftMotor::getAppliedOutput, leftMotor::getBusVoltage},
-                (values) -> inputs.appliedVolts = values[0] * values[1]);
-                ifOk(leftMotor, leftMotor::getOutputCurrent, (value) -> inputs.currentAmps = value); 
-            ifOk(
-            rightMotor,
-                 new DoubleSupplier[] {rightMotor::getAppliedOutput, rightMotor::getBusVoltage},
-                (values) -> inputs.appliedVolts = values[0] * values[1]);
-                ifOk(rightMotor, rightMotor::getOutputCurrent, (value) -> inputs.currentAmps = value);          
-        }
+        // Removed duplicate method
         
         
         @Override
@@ -43,6 +35,16 @@ package frc.robot.subsystems.Intake;
             leftMotor.setVoltage(volts);
             rightMotor.setVoltage(volts);
         }
+
+    private boolean isSensorHit(DigitalInput sensor) {
+            return sensor.get(); //put in actual threshold
         }
-        
-        
+    public void updateInputs(IntakeIOInputs inputs) {
+           if (isSensorHit(leftSensor) || isSensorHit(rightSensor)) {
+            inputs.velocityRadPerSec = 0;
+    } else {
+            ifOk(leftMotor, leftEncoder::getPosition, value -> inputs.positionRad = value);
+            ifOk(rightMotor, rightEncoder::getPosition, value -> inputs.positionRad = value); 
+    }
+}
+}     

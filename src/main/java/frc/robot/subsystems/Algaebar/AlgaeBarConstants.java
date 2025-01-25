982ff9f5004cda0a0a37180223013d52b1423902
Vortex547 <gashni26@spaldinghs.org>
2025-01-25 12:35:46 -0500

    Algae Bar Sensors

--- src/main/java/frc/robot/subsystems/Algaebar/AlgaeBarConstants.java
+++ src/main/java/frc/robot/subsystems/Algaebar/AlgaeBarConstants.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems.Algaebar;
 
 public class AlgaeBarConstants {
-    public static final int kAlgaeBarCoralCanID = 50;
+    public static final int kAlgaeBarCoralCanID = 50; //CanID names subject to change
     public static final int kAlgaeBarRotateCanID = 51;
 
     public static final double minRotation = 0; //Pretty much all of these are TBD since I still have no clue what I'm doing
     public static final double maxRotation = 0; 
+
+    public static final int algaeBarSensorOne = 0; //sensor names subject to change
+    public static final int algaeBarSensorTwo = 0;
 
     public static final int forwardDrive = 1;
 
--- src/main/java/frc/robot/subsystems/Algaebar/AlgaeBarIOSpark.java
+++ src/main/java/frc/robot/subsystems/Algaebar/AlgaeBarIOSpark.java
@@ -8,16 +8,21 @@ import static frc.robot.subsystems.Algaebar.AlgaeBarConstants.kAlgaeBarCoralCanI
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 
 public class AlgaeBarIOSpark implements AlgaeBarIO {
-    private final SparkMax rotateMotor = new SparkMax(kAlgaeBarRotateCanID, MotorType.kBrushless);
+    private final SparkMax rotateMotor = new SparkMax(kAlgaeBarRotateCanID, MotorType.kBrushless); //change CanID names as appropriate
     private final RelativeEncoder rotateEncoder = rotateMotor.getEncoder();
 
     private final SparkMax coralMotor = new SparkMax(kAlgaeBarCoralCanID, MotorType.kBrushless);
     private final RelativeEncoder coralEncoder = coralMotor.getEncoder();
+
+    public DigitalInput firstSensor = new DigitalInput(AlgaeBarConstants.algaeBarSensorOne); //change sensor names as appropriate
+    public DigitalInput secondSensor = new DigitalInput(AlgaeBarConstants.algaeBarSensorTwo);
 
     public AlgaeBarIOSpark(){
         //could do motor configuration here
@@ -33,13 +38,20 @@ public class AlgaeBarIOSpark implements AlgaeBarIO {
                 (values) -> inputs.appliedVolts = values[0] * values[1]);
         ifOk(rotateMotor, rotateMotor::getOutputCurrent, (value) -> inputs.currentAmps = value);   
         
-        ifOk(coralMotor, coralEncoder::getPosition, (value) -> inputs.positionRad = value); //only 
+        ifOk(coralMotor, coralEncoder::getPosition, (value) -> inputs.positionRad = value); 
         ifOk(coralMotor, coralEncoder::getVelocity, (value) -> inputs.velocityRadPerSec = value);
         ifOk(
             coralMotor,
                 new DoubleSupplier[] {coralMotor::getAppliedOutput, coralMotor::getBusVoltage},
                 (values) -> inputs.appliedVolts = values[0] * values[1]);
         ifOk(coralMotor, coralMotor::getOutputCurrent, (value) -> inputs.currentAmps = value);   
+
+        if (isSensorHit(firstSensor) || isSensorHit(secondSensor)) {
+            inputs.velocityRadPerSec = 0;
+        } else {
+            ifOk(rotateMotor, rotateEncoder::getPosition, value -> inputs.positionRad = value);
+            ifOk(coralMotor, coralEncoder::getPosition, value -> inputs.positionRad = value);
+    }
     } 
 
     @Override
@@ -48,4 +60,8 @@ public class AlgaeBarIOSpark implements AlgaeBarIO {
 
             coralMotor.setVoltage(volts);
         }
-    }
+
+        private boolean isSensorHit(DigitalInput sensor) {
+            return sensor.get();
+        }
+ }

bb830d6f87574341bd46923fb07094684cb95281
mattheW0324 <lindquistma27@spaldinghs.org>
2025-02-08 10:20:59 -0500

    Feat: Battery Voltage

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -147,5 +148,14 @@ public class Robot extends LoggedRobot {
     /** This function is called periodically during test mode. */
     @Override
     public void testPeriodic() {
+    }
+    public void batteryVoltage() {
+        // Get the battery voltage
+        double voltage = RobotController.getBatteryVoltage();
+        if (voltage < 10.0) {
+            // Log a warning if the battery voltage is low
+            Logger.recordOutput("Voltage", voltage);
+            
+        }
     }
 }

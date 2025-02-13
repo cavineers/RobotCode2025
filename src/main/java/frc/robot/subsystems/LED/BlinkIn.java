b267529f16c95ac91ab39f09aa6f3ebeef27f380
Vortex547 <gashni26@spaldinghs.org>
2025-02-13 18:38:52 -0500

    LED Code

---
+++ src/main/java/frc/robot/subsystems/LED/BlinkIn.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BlinkIn extends SubsystemBase {
+    
+    public static Spark blinkinLeft;
+    public static Spark blinkinRight;
+
+    public Blinkin() {
+        blinkinLeft = new Spark(0);
+        blinkinRight = new Spark(1);
+    }
+
+    public void lightsRainow() {
+        blinkinLeft.set(-0.99);
+        blinkinRight.set(-0.99);
+    }
+
+    public void lightsDefault() {
+        blinkinLeft.set(-0.39);
+        blinkinRight.set(-0.39);
+    }
+
+    public void lightsFire() {
+        blinkinLeft.set(-0.59);
+        blinkinRight.set(-0.59);
+    }
+
+    public void lightsChase() {
+        blinkinLeft.set(0.01);
+        blinkinRight.set(0.01);
+    }
+
+    public void lightsOcean() {
+        blinkinLeft.set(-0.41);
+        blinkinRight.set(-0.41);
+    }
+
+    public void lightsOrange() {
+        blinkinLeft.set(0.65);
+        blinkinRight.set(0.65);
+    }
+
+    public void endToEnd() {
+        blinkinLeft.set(0.47);
+        blinkinRight.set(0.47);
+    }
+
+}

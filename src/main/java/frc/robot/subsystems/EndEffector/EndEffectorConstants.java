35a303f25d656ab9f06668f9ff0157787ba8cb45
Sam <58117545+Sam-Malta@users.noreply.github.com>
2025-02-25 16:35:48 -0500

    Fix: revert speed back down on shooter

--- src/main/java/frc/robot/subsystems/EndEffector/EndEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/EndEffector/EndEffectorConstants.java
@@ -8,8 +8,8 @@ public class EndEffectorConstants {
     public static final int kCoralPresentIR = 1;
     public static final int kCoralLoadedLimit = 2;
 
-    public static final double kEndEffectorShootSpeed = 0.5;
-    public static final double kEndEffectorIntakeSpeed = 0.5;
+    public static final double kEndEffectorShootSpeed = 0.35;
+    public static final double kEndEffectorIntakeSpeed = 0.35;
 
     // Motor Configuration
     public static final boolean kInverted = false;

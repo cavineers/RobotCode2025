62607ff83badd4cd00a34feba3819caffe450948
Logan Bechtel <bechtello25@spaldinghs.org>
2025-03-01 12:14:58 -0500

    Fixes

--- src/main/java/frc/robot/subsystems/Lights/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights/Lights.java
@@ -1,8 +1,12 @@
 package frc.robot.subsystems.Lights;
 
 import static frc.robot.subsystems.Lights.LightsConstants.*;
+
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.DistanceUnit;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -10,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.Elevator.ElevatorConstants.ElevatorState;
 import java.util.function.Supplier;
+
 
 import org.littletonrobotics.junction.Logger;
 
@@ -20,23 +25,19 @@ public class Lights extends SubsystemBase {
     private Supplier<Boolean> isNearStation;
 
     private AddressableLED ledStrip = new AddressableLED(kPWMPort);
-    private AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(60);
+    private AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(25);
 
+    private final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
 
+    // Our LED strip has a density of 120 LEDs per meter
     public Lights() {
         this.ledStrip.setLength(this.ledBuffer.getLength());
 
         // Set the data
+        this.m_rainbow.applyTo(ledBuffer);
         this.ledStrip.setData(this.ledBuffer);
         this.ledStrip.start();
 
-        LEDPattern red = LEDPattern.solid(Color.kRed);
-
-        // Apply the LED pattern to the data buffer
-        red.applyTo(this.ledBuffer);
-
-        // Write the data to the LED strip
-        this.ledStrip.setData(this.ledBuffer);
     }
 
 
--- src/main/java/frc/robot/subsystems/Lights/LightsConstants.java
+++ src/main/java/frc/robot/subsystems/Lights/LightsConstants.java
@@ -3,5 +3,5 @@ package frc.robot.subsystems.Lights;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class LightsConstants {
-    public static final int kPWMPort = 0;
+    public static final int kPWMPort = 5;
 }
